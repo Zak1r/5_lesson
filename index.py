d44183c39087f86247d8213b3fe59243920ad8ae
Zokirzhon Ikramzhanov <zokirzhon99@gmail.com>
2021-11-13 20:53:06 +0600

    solved task for and or operators

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-name = input('enter your name: ')
+'''name = input('enter your name: ')
 surname = input ('enter your 2nd name: ')
 
 print ('Hello, '+name + ' ' + surname)
@@ -6,5 +6,41 @@ print (f'Hello, {name} {surname}')
 #форматированные строки
 print('hello, {} {}'.format(name,surname))
 #placeholder
-#hello, 1st name 2nd name
+#hello, 1st name 2nd name'''
 
+"""num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2st number: '))
+print(f'{num1} + {num2} = {num1+num2}')
+print(f'{num1} - {num2} = {num1-num2}')
+print(f'{num1} / {num2} = {num1/num2}')
+print(f'{num1} * {num2} = {num1*num2}')"""
+
+# print("I'm Zakir")
+# print('I\'m Zakir')
+# print('I am Zakir')
+#\n new line
+#\t tab
+
+# or (^) operator or(или) цель найти true
+# and логическая сложения умножение (дизьнкция) цель найти false
+#or not инверсия 
+
+# print(True or False)
+# print(True ^ False )
+
+'''login ='admin'
+password='admin'
+
+if login == 'admin' and password == 'admin':
+    print('welcome')
+else: 
+    print('bye!')
+'''
+
+login ='admin'
+password='admin'
+
+if login == 'admin' and password == 'admin':
+    print('welcome')
+else: 
+    print('bye!')
