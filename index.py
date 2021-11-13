01654abaeab55ff89742f211cadc3336e44582eb
Zokirzhon Ikramzhanov <zokirzhon99@gmail.com>
2021-11-13 20:02:27 +0600

    added code related to string formatting

---
+++ index.py
@@ -0,0 +1,10 @@
+name = input('enter your name: ')
+surname = input ('enter your 2nd name: ')
+
+print ('Hello, '+name + ' ' + surname)
+print (f'Hello, {name} {surname}') 
+#форматированные строки
+print('hello, {} {}'.format(name,surname))
+#placeholder
+#hello, 1st name 2nd name
+
